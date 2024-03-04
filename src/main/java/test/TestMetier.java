013f243824b17161eff28f2ef13e7767b57ff9c4
sarrajerbi <sarrajerbi99@gmail.com>
2024-03-04 21:51:38 +0100

    commit

---
+++ src/main/java/test/TestMetier.java
@@ -0,0 +1,13 @@
+package test;
+import java.util.List;
+
+import metier.MetierImpl;
+import metier.Produit;
+public class TestMetier {
+	public static void main(String[] args) {
+		MetierImpl metier= new MetierImpl();
+		List<Produit> prods = metier.getProduitsParMotCle("HP");
+	for (Produit p : prods)
+		System.out.println(p.getNomProduit());
+}
+}
